ec36a2418008a079e9aae65fae5738a264fa7875
Ze <elawrence.bda@gmail.com>
2024-05-23 02:13:59 -0400

    Added to README, fixed links on footer

--- Main/src/components/Footer.jsx
+++ Main/src/components/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => (
 <Card className='footer'>
   <Card.Body>
     <Button className="m-1 p-1"  variant="secondary"><a className="text-white" target="_blank"  href="https://github.com/ZeDevGit">GitHub</a></Button>
-    <Button className="m-1 p-1" variant="secondary"><a className="text-white" target="_blank"  href="">LinkedIn</a></Button>
-    <Button  className="m-1 p-1" variant="secondary"><a className="text-white"  target="_blank" href="">Medium</a></Button>
+    <Button className="m-1 p-1" variant="secondary"><a className="text-white" target="_blank"  href="https://www.linkedin.com/in/edward-lawrence-22876830b/">LinkedIn</a></Button>
+    <Button  className="m-1 p-1" variant="secondary"><a className="text-white"  target="_blank" href="https://stackexchange.com/users/32440912/zedevgit">StackOverflow</a></Button>
   </Card.Body>
 </Card>
 
